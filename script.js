fa3cf785748efd1a00fe616fcee560e7dd2ec5a6
scottbishop <sa_bishop@comcast.net>
2021-07-08 22:01:40 -0400

    7-8-21

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 "use strict"
+let form = document.querySelector("form");
+//let balance = 0;
+let span = document.getElementById("balance");
+let budget = new FormData(form);
+// let balance = document.querySelector("span").innerText;
+// let budgetInput = document.getElementById("budgetAmt").valueAsNumber;
+
+form.addEventListener("submit", (event)=>{
+    //prevent refresh
+    event.preventDefault();
+    document.querySelector("span").innerText = document.getElementById("budgetAmt").valueAsNumber;
+    console.log(document.getElementById("budgetAmt").valueAsNumber);
+});
+//add budget into the balance
+
+//amount added to money spent
+
+//link amount to category
+
+//subtract amount from balance
+
+
+
+
+
+
+
+// Create starter values
+// let expenses = []; // Array of expense objects
+// let monthlyIncome = 0; // Numberical value
+// let expenseTotal = 0; // Numebrical value
+// let balance = 0; // Numerical value
+
+// // Create references to monthly budget elements
+// let monthlyBudget = document.getElementById("monthly_budget");
+// let incomeInput = document.getElementById("income_input");
+// let updateBudgetButton = document.getElementById("update_budget_button");
+
+// //Build a function that displays the budget entered by the user
+// function updateBudget(event) {
+//     event.preventDefault(); // Prevent refresh
+//     // Store user data entry into monthlyIncome
+//     // Input values are strings; need to parse into an integer
+//     monthlyIncome = parseInt (incomeInput.value);
+//     // Display input in the app
+//     monthlyBudget.innerText = "$" + incomeInput.value;
+//     // Reset the form
+//     incomeInput.value = "";
+//     // Update the balance
+//     updateBalance();
+// }
