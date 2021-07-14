b75d6c7eccbf38a833a9d771f0ce84121ec12aaa
Matthew Boeving <mboeving1@gmail.com>
2021-07-13 21:18:34 -0400

    did this work

--- script.js
+++ script.js
@@ -1,73 +1,34 @@
-"use strict"
-let form = document.querySelector("form");
-//let pushButton = document.getElementsByClassName("listener")
-//let balance = 0;
-let span = document.getElementById("balance");
-let spanTwo = document.getElementById("moneySpent");
-let budget = new FormData(form);
-// let balance = document.querySelector("span").innerText;
-// let budgetInput = document.getElementById("budgetAmt").valueAsNumber;
+"use strict";
 
-// form.addEventListener("submit", (event)=>{  
-//     //prevent refresh
-     //event.preventDefault();
-     //     console.log(document.getElementById("budgetAmt").valueAsNumber);
-     // });
-     //add budget into the balance
-     document.querySelectorAll(".listener").forEach(item => {
-         item.addEventListener("submit", event => {
-             event.preventDefault();
-             document.querySelector("span").innerText = +(document.getElementById("budgetAmt").valueAsNumber);
-            })
-        })
+function addMoney(){
+    var totalBudget = parseInt(document.getElementById("totalBudget").innerHTML);
+    var currentBalance = parseInt(document.getElementById("currentBalance").innerHTML);
+        var money = parseInt(document.getElementById("money").value);
+        if (money == "" || isNaN(money)){
+            alert("Please enter a valid number");
+        }
+        else{
+            totalBudget += money;
+            currentBalance += money;
+            document.getElementById("totalBudget").innerHTML = totalBudget;
+            document.getElementById("currentBalance").innerHTML = currentBalance;
+        }
+}
 
-        document.querySelectorAll(".listener").forEach(item => {
-            item.addEventListener("submit", event => {
-                //event.preventDefault();
-                spanTwo.innerText = document.getElementById("moneyInput").valueAsNumber;
-               })
-           })
+function addExpense(){
+    var currentBalance = parseInt(document.getElementById("currentBalance").innerHTML);
+    var expense = document.getElementById("categoryName").value;
+        var expenseAmt = parseInt(document.getElementById("expenseAmt").value);
 
-        
-    // document.getElementById("submitTwo").addEventListener("submit", ()=>{  
-    //     spanTwo.innerText = document.getElementById("moneyInput").valueAsNumber;
-    //     console.log(document.getElementById("moneyInput").valueAsNumber)
-    //     });
-//amount added to money spent
-
-//link amount to category
-
-
-
-//subtract amount from balance
-
-
-
-
-
-
-
-// Create starter values
-// let expenses = []; // Array of expense objects
-// let monthlyIncome = 0; // Numberical value
-// let expenseTotal = 0; // Numebrical value
-// let balance = 0; // Numerical value
-
-// // Create references to monthly budget elements
-// let monthlyBudget = document.getElementById("monthly_budget");
-// let incomeInput = document.getElementById("income_input");
-// let updateBudgetButton = document.getElementById("update_budget_button");
-
-// //Build a function that displays the budget entered by the user
-// function updateBudget(event) {
-//     event.preventDefault(); // Prevent refresh
-//     // Store user data entry into monthlyIncome
-//     // Input values are strings; need to parse into an integer
-//     monthlyIncome = parseInt (incomeInput.value);
-//     // Display input in the app
-//     monthlyBudget.innerText = "$" + incomeInput.value;
-//     // Reset the form
-//     incomeInput.value = "";
-//     // Update the balance
-//     updateBalance();
-// }
+        if ( expenseAmt == "" || isNaN(expenseAmt)){
+            alert("Please enter a valid number");
+        }
+        else if(expenseAmt <= currentBalance){
+            currentBalance -= expenseAmt;
+            document.getElementById("currentBalance").innerHTML = currentBalance;
+            document.getElementById(expense).innerHTML = expenseAmt;
+        }
+        else{
+            alert("Sorry, you are currently broke");
+        }
+} 
